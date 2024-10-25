8f968b4e2055809965d6e8fd443379aa27cffcb8
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-10-25 17:49:59 +0530

    jumps in  for loop

--- control constructs/Looping statements/forLoop.py
+++ control constructs/Looping statements/forLoop.py
@@ -1,2 +1,7 @@
 for i in range(1, 11):
     print("2 X "+str(i)+" = " + str(2 * i))
+
+print("==== STEPS IN FOR LOOP ====")
+
+for i in range(1, 21, 3):   # The third parameter works as a "jump to" i.e. after 1 the 3rd jump is 3
+    print(i)
