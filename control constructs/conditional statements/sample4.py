5c3cc260b6cfdc49ec573329d91ca0cb999e20c9
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-10-25 17:46:08 +0530

    if else ladder

---
+++ control constructs/conditional statements/sample4.py
@@ -0,0 +1,11 @@
+# if else ladder
+free_tonight = True
+friends_available = False
+
+if free_tonight:
+    if friends_available:
+        print("Have a dinner party")
+    else:
+        print("Watch a movie")
+else:
+    print("Continue with your task")
