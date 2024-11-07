3f2c9df9be85f100c974ac6f9ce771096c27079b
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-11-07 19:52:25 +0530

    tuples

---
+++ list_and_tuples/Tuples.py
@@ -0,0 +1,9 @@
+# Tuples in python
+# It is a built-in data structure in python that allow us to create an immutable sequence of values
+tup = (45, 50, 34, 48, 39, 45)
+print(tup)
+
+# methods in tuples
+print(tup.index(39))    # returns the index number of the element
+print(tup.count(45))    # returns the number of occurrence of that element
+
