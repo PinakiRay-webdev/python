f2901f61a5ea9b1ca87b4a7574560e6fec3acec3
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-11-08 11:56:38 +0530

    methods of dictionary

--- Dictionaries_and_sets/dictionary.py
+++ Dictionaries_and_sets/dictionary.py
@@ -6,11 +6,27 @@ employee = {
     "languages known": ["English", "Odia", "Hindi", "Bengali"]
 }
 
+# to get the particular key we can write :
+    # 1-> dictionary_name["key"]
+    # 2-> dictionary_name.get("key")
+
+#The first one is not recomended because that will throw error in case if the key is not present. The second will give null value if the key is not present. So that our rest of the code can continue with out any interuption
+
+
 print(employee)
-print("Employee role:", employee["role"])
+print("Employee role:", employee.get("role"))
 print("Languages known by employee:", employee["languages known"])
 
 # Adding a new key and it's value in dictionary
 
 employee["city"] = "Hyderabad"
+print(employee)
+
+print("=================")
+# Major Methods in dictionaries
+print(employee.keys())      # return all keys
+print(employee.values())    # return all values
+print(employee.items())     # Returns all (key , value) pairs as tuple
+
+print(employee.update({"Date of birth": "15th june 2001"}))     # updates the dictionary by taking a new dic
 print(employee)
